5feff530a0178fa9c95487064103dcf279c792bf
oboard <oboard@outlook.com>
2023-12-30 21:20:30 +0800

    init

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="carousel carousel-vertical h-screen w-screen">
+        <div>
+            <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Random image">
+        </div>
+        <div>
+            <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Random image">
+        </div>
+    </div>
+</template>
+
+<style>
+.carousel > * {
+    @apply carousel-item h-screen w-screen;
+}
+</style>
